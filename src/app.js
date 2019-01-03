e01a5d948f9d4b1190dd918600e8fc8ac5f30913
Alexander Rao <alexhrao@gmail.com>
2019-01-03 17:51:58 -0600

    Add testing files

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express()
+const port = 3000
+app.set("view engine", "pug");
+console.log("Hello, world!");
+app.get('/', (req, res) => res.render("index"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
