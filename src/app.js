3bc3cf1c2ef71640f2cfc3d72f4c675b6d91ac50
Alexander Rao <alexhrao@gmail.com>
2019-01-05 14:03:08 -0600

    Set up gifts to reflect example

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from "express";
-
+import giftRoutes from "./gifts";
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => res.send({"hello": "world"}));
+
+const routes = express.Router();
+routes.get("/", (req, res) => res.send({"Hi": "world"}));
+
+routes.use("/gifts", giftRoutes);
+app.use("/", routes);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
